d233d13a670f622119bf43afa493c1aca040d04a
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-02-07 23:00:33 +0530

    converting doublt to float data type

---
+++ convert1.java
@@ -0,0 +1,8 @@
+public class convert1 {
+    public static void main(String[] args) {
+        double num=10.0009899989;
+        float num2=(float)num;
+        System.out.println(num2);
+
+    }
+}
