a6d860f1dc06145e09b4af91085e2aef191995c8
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-02-07 22:47:52 +0530

    adding short,int,long values

---
+++ addshort_int_long.java
@@ -0,0 +1,9 @@
+public class addshort_int_long {
+    
+    public static void main(String[] args){
+        short num1=12;
+        int num2=22;
+        long num3=456;
+        System.out.println(num1+num2+num3);
+    }
+}
