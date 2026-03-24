5d0631e226bbf720473967aa1a0536901f9bcb32
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-03-24 21:22:45 +0530

    finding parameter for an rectangle

---
+++ largrarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class largrarray {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+      
+       int length=sc.nextInt();
+       int breadth=sc.nextInt();
+        int parameter=2*(length+breadth);
+
+System.out.println("parameter:"+parameter);
+    }
+}
