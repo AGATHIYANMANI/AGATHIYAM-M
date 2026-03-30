04eba8531e078e01035ca9eb368775a84df37c64
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-03-30 20:58:03 +0530

    checking whether the number is krishnamurthy number or not
    A Krishnamurthy number is a number whose sum of the factorial of its digits is equal to the original number.
    
    Example:
    Number = 145
    Calculation:
    1! + 4! + 5! = 1 + 24 + 120 = 145
    Since result = original number → ✅ Krishnamurthy number

---
+++ krishnamurthy.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class krishnamurthy{
+    int factorial(int num){
+        if(num==0||num==1){
+            return 1;
+        }
+        else{
+            return num*factorial(num-1);
+        }
+    }
+    int cal(int num){
+    int org=num;
+    int sum=0;
+    while(org>0){
+        int temp=org%10;//123..3
+        sum+=factorial(temp);
+        org/=10;//..12
+    
+        }
+        return sum;
+    }
+    
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sec=num;
+        krishnamurthy kr=new krishnamurthy();
+        int result=kr.cal(num);
+        if(result==sec){
+            System.out.println("It is krishnamurthy number");
+        }
+        else{
+            System.out.println("It is not krishnamurthy number");
+        }
+    }
+}
