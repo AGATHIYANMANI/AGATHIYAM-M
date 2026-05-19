bf7821e8295519a49f70b96de3bc273db486e8bc
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-05-19 18:12:38 +0530

    asking input

---
+++ input3.java
@@ -0,0 +1,8 @@
+import java.io.Console;
+public class input3{
+    public static void main(String [] args){
+        Console cn=System.console();
+        String name=cn.readLine();
+        System.out.println("SENTENCE:"+name);
+}
+}
