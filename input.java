833b999331deaff42c8796f0b39c9be930f1b72a
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-05-19 18:11:26 +0530

    input asking
    asking input by using Scanner class

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+    public class input{
+        public static void main(String [] args){
+            System.out.println("Enter sentence:");
+    Scanner sc=new Scanner(System.in);
+    String character=sc.nextLine();
+    System.out.println("name:"+ character);
+
+}
+    }
