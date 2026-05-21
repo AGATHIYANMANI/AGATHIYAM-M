10ff500ffd6125902be6fd68376ea8658d8e636f
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-05-21 10:29:51 +0530

    output asking

---
+++ output5.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class output5 {
+    public static void main(String[] args){
+        PrintWriter pw=new PrintWriter(System.out);
+        pw.println("hi");
+        pw.flush();
+    }
+    
+}
