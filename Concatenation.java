94e13f1ac765cc939f9af2d25f478593c091efcb
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-04 20:23:51 +0530

    Calculating Concatenation of two array using Scanner method

---
+++ Concatenation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Concatenation{
+    public static void main(String [] args){
+        System.out.println("Enter length of array:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int[] array=new int[n];
+         for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+
+         }
+         int[] sum=new int[2*n];
+         for(int i=0;i<n;i++){
+            sum[i]=array[i];
+            sum[i+n]=array[i];
+
+         }
+         System.out.println("Concatenation array is:");
+         System.out.println(Arrays.toString(sum));
+
+    }
+}
