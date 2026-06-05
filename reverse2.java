40b1c95a40370cd573eb00082ae29320f57ecea4
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-05 16:27:45 +0530

    reversing a number using console and println method

--- reverse2.java
+++ reverse2.java
@@ -3,14 +3,12 @@ public class reverse2{
 public static void main(String [] args) throws IOException{
    Console cs=System.console();
     System.out.println("Enter number:");
-
     int num=Integer.parseInt(cs.readLine());
     int las=0;
     while(num>0){
         int a=num%10;
         las=(las*10)+a;
         num/=10;
-
     }
    System.out.println("The reversed num is:"+las);
  
