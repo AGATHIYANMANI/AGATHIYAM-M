aef2a975d8de8280ffede4763248fccceb15f048
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-13 18:28:29 +0530

    Calculating compound intrest using Scanner method

--- compoundintrest.java
+++ compoundintrest.java
@@ -4,7 +4,6 @@ public class compoundintrest {
         double cc=(P* Math.pow(1+(R/100),y))-P;
         System.out.println("Compound intrest:"+cc);
     }
-    
 public static void main(String [] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter principle ammount:");
