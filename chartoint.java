c47f67e03e5d303aea4d81a567540708191b8849
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-29 18:24:43 +0530

    converting char to int using scanner method

--- chartoint.java
+++ chartoint.java
@@ -1,14 +1,14 @@
+
 import java.util.*;
-public class chartoint {
-    void cal(char a){
-        System.out.println("character:"+a);
-        int b=a;
-        System.out.println("integer:"+b);
-    }
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        char a=sc.next().charAt(0);
-        chartoint cc=new chartoint();
-        cc.cal(a);
+
+public class chartoint{
+    public static void main(String [] args) {
+       Scanner sc=new Scanner(System.in);
+        char name=sc.next().charAt(0);
+        int convert=(int)name;
+        System.out.printf("Integer of character %c is %d",name,convert);
+       
+
+
     }
 }
