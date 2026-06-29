f13d016a8031c58058b8889284f006c56252cbd5
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-29 18:20:37 +0530

    converting char to int usinf console method

--- chartoint2.java
+++ chartoint2.java
@@ -1,17 +1,14 @@
-import java.io .*;
 
-public class chartoint2 {
-    void cal(char a) throws IOException{
-      System.out.println("character:"+a);
-      int b=a;
-      System.out.println("integer:"+b);
-      
-    }
+import java.io.*;
+
+public class chartoint2{
     public static void main(String [] args) throws IOException{
        Console cs=System.console();
-        char a=cs.readLine().charAt(0);
-        chartoint2 cc=new chartoint2();
-        cc.cal(a);
+        char name=cs.readLine().charAt(0);
+        int convert=(int)name;
+        System.out.println("Integer of character "+name + " is "+ convert);
+       
+
 
     }
 }
