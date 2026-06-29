2fa8ece5f662f152e1f60b5394b51389ce68259c
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-29 18:22:53 +0530

    converting char to int using console with printf

--- chartoint3.java
+++ chartoint3.java
@@ -1,17 +1,14 @@
-import java.io .*;
 
-public class chartoint3 {
-    void cal(char a) throws IOException{
-      System.out.printf("\ncharacter:%c",a);
-      int b=a;
-      System.out.printf("\ninteger:%d",b);
-      
-    }
+import java.io.*;
+
+public class chartoint3{
     public static void main(String [] args) throws IOException{
        Console cs=System.console();
-        char a=cs.readLine().charAt(0);
-        chartoint3 cc=new chartoint3();
-        cc.cal(a);
+        char name=cs.readLine().charAt(0);
+        int convert=(int)name;
+        System.out.printf("Integer of character %c is %d",name,convert);
+       
+
 
     }
 }
