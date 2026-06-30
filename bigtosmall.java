3868f5b30fa326e18f4dbb1b4d93306163869ba2
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-30 20:22:23 +0530

    converting capital letter to small letter using scanner method

---
+++ bigtosmall.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class bigtosmall{
+    public static void main(String [] args){
+    Scanner sc=new Scanner(System.in);
+    char character =sc.next().charAt(0);
+    int number=((int)character+32);
+    char result=(char)number;
+
+System.out.println("The small of "+character+" is "+result);
+    }
+}
