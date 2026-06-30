c1664e23a821fba553137e6a84a443f183760916
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-06-30 20:29:27 +0530

    converting capital letter to small letter using buffered method

--- bigtosmall1.java
+++ bigtosmall1.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class bigtosmall1 {
-    
     public static void main(String [] args) throws IOException{
    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
    BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
