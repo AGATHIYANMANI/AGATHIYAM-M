6969c054e8345639285c7e14c328bb057f0cd7fd
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-07-10 21:31:33 +0530

    printing reverse of triangle using console method

---
+++ patt2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+
+public class patt2 {
+     public static void main(String[] args) throws IOException{
+        Console cs=System.console();
+        int rows = 5;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+               
+            }
+            System.out.println("\n ");
+        
+    }
+}
+}
