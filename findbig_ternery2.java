73ce4fa43df5de997e17334603b125956ccab738
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-07-16 21:16:36 +0530

    finding big of three number using ternery with console method

---
+++ findbig_ternery2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class findbig_ternery2 {
+    public static void main(String[] args){
+       Console cs=System.console();
+        int a=Integer.parseInt(cs.readLine());
+        int b=Integer.parseInt(cs.readLine());
+        int c=Integer.parseInt(cs.readLine());
+        String result=(a>b&&a>c)?a+" is big":(b>c&&b>a)?b+" is big":c+" is big";
+        System.out.println("Result : "+result);
+    }
+}
