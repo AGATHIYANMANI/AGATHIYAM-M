0a941030c5a864c810f3adff8820dd0ab2702c81
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-07-18 18:21:29 +0530

    printing  N number of prime number using Scanner class

---
+++ primenumberN.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class primenumberN {
+    public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+
+for(int i=2;i<N;i++){
+    int count=0;
+    for(int j=2;j<i;j++){
+        if(i%j==0){
+            count++;
+        
+            break;
+        }
+    }
+    if(count==0){
+    System.out.println(i);
+}
+}
+
+    }
+}
