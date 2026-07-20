3555caecc6561b86251a70f511deda2a0ea2ce3e
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-07-20 21:45:17 +0530

    removing space between sentence using buffered method

---
+++ remove2.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class remove2 {
+       public static void main(String [] args){
+        Console cs=System.console();
+    
+     String sentence=cs.readLine();
+     String removed=sentence.replace(" ","");
+     System.out.println("Given Sentence: "+sentence);
+     System.out.println("Removed Sentence: "+removed);
+    }
+}
