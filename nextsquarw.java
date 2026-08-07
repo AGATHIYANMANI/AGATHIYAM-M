9649fe21dfbb79f502fab06946c223c9fec67e5c
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-08-07 21:05:48 +0530

    finding the next square of given number using scanner method

---
+++ nextsquarw.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class nextsquarw {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int result=num*2;
+        System.out.println(result); 
+    }
+}
