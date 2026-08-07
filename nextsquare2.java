ce00928ddbc8fc499d69d15e2acdb7baa5bf5796
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-08-07 21:06:53 +0530

    finding the next square of given number using console method

---
+++ nextsquare2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class nextsquare2 {
+     public static void main(String [] args)throws IOException{
+        Console cs=System.console();
+        int num=Integer.parseInt(cs.readLine());
+        int result=num*2;
+        System.out.println(result); 
+    }
+}
