0caad5bb36a7ee4307dd3ac698b8645cb4282703
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-08-18 20:55:16 +0530

    printing the first string all character except the second string character using console method

---
+++ rep2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class rep2 {
+   public static void main(String[] args) {
+       Console cs=System.console();
+       String S1=cs.readLine();
+       int n=S1.length();
+       String S2=cs.readLine();
+       int p=S2.length();
+       int total=0;
+       for(int i=0;i<n;i++){
+           int count=0;
+           for(int j=0;j<p;j++){
+               if(S1.charAt(i)==S2.charAt(j)){
+                  count++;
+                  break;
+               }
+           }
+           if(count==0){
+               System.out.print(S1.charAt(i));
+               total++;
+           }
+           }
+           if(total==0){
+               System.out.println("-1");
+               return;
+       }
+    } 
+}
