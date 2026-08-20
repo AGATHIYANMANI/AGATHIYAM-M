f8c3a7bf380eddcd574fc942eee0d4cabc9ee4f7
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-08-20 20:49:15 +0530

    in an array,printing the one even element if all all the elements are odd or printing odd elements using console method

--- count2.java
+++ count2.java
@@ -1,31 +1,41 @@
-import java.io.Console;
+import java.io.*;
 
 public class count2 {
-     public static void main(String [] args){
-       Console cs=System.console();
-        System.out.println("Enter length of an array");
-        int n=Integer.parseInt(cs.readLine());
-        System.out.println("Enter value to count:");
-        char k=cs.readLine().charAt(0);
-        char sentence[]=new char[n];
-        System.out.println("Enter value one by one");
-        for(int i=0;i<=n-1;i++){
-            sentence[i]=cs.readLine().charAt(0);
+    public static void main(String[] args) {
+        Console cs = System.console();
+        int n = Integer.parseInt(cs.readLine());
+        int[] N = new int[n];
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            N[i] = Integer.parseInt(cs.readLine());
         }
-        int count=0;
-        for(int j=0;j<=n-1;j++){
-            if(k==sentence[j]){
-               count++;
-               continue;
-                            }
-                            
+        for (int j = 0; j <= n - 1; j++) {
+            if (N[j] % 2 == 0) {
+                even++;
+            } else if (N[j] % 2 == 1) {
+                odd++;
+            }
         }
-        
-        if(count!=0){
-            System.out.println("Total count of "+k+" is "+count);
-        }
-        else{
-            System.out.println("Element cannot found!");
+        for (int k = 0; k <= n - 1; k++) {
+            if (even > odd) {
+                if (N[k] % 2 == 1) {
+                    System.out.println(N[k]);
+                    return;
+                } else {
+                    System.out.println("-1");
+                    return;
+                }
+
+            } else if (odd > even) {
+                if (N[k] % 2 == 0) {
+                    System.out.println(N[k]);
+                    return;
+                } else {
+                    System.out.println("-1");
+                    return;
+                }
+            }
         }
 
     }
