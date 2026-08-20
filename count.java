8b14d3dae0c584a86f1fae812ba338707ca9f382
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-08-20 20:48:29 +0530

    in an array,printing the one even element if all all the elements are odd or printing odd elements using scanner method

--- count.java
+++ count.java
@@ -1,31 +1,43 @@
-import java.util.*;
+import java.util.Scanner;
 public class count {
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter length of an array");
-        int n=sc.nextInt();
-        System.out.println("Enter value to count:");
-        char k=sc.next().charAt(0);
-        char sentence[]=new char[n];
-        System.out.println("Enter value one by one");
-        for(int i=0;i<=n-1;i++){
-            sentence[i]=sc.next().charAt(0);
-        }
-        int count=0;
-        for(int j=0;j<=n-1;j++){
-            if(k==sentence[j]){
-               count++;
-               continue;
-                            }
-                            
-        }
-        
-        if(count!=0){
-            System.out.println("Total count of "+k+" is "+count);
-        }
-        else{
-            System.out.println("Element cannot found!");
-        }
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] N=new int[n];
+    int even=0;
+    int odd=0;
+  for(int i=0;i<=n-1;i++){
+    N[i]=sc.nextInt();
+}
+for(int j=0;j<=n-1;j++){
+if(N[j]%2==0){
+even++;
+}else if(N[j]%2==1){
+odd++;
+}
+}for(int k=0;k<=n-1;k++){
+   if(even>odd){
+  if(N[k]%2==1){
+System.out.println(N[k]);
+return;
+}
+else{
+System.out.println("-1");
+return;
+}
 
     }
+    else if(odd>even){
+        if(N[k]%2==0){
+            System.out.println(N[k]);
+            return;
+        }
+        else{
+            System.out.println("-1");
+            return;
+        }
+    }
+}
+
+}
 }
