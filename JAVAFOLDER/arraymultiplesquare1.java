e6bf3d1ac744333a717d06ea3bcc2005308e615c
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-09-22 23:02:40 +0530

    Refactor input/output to use BufferedReader and BufferedWriteriven an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order using buffered method
    Replaced Scanner with BufferedReader and BufferedWriter for input and output handling.

---
+++ JAVAFOLDER/arraymultiplesquare1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class arraymultiplesquare {
+    public int[] sortedSquares(int[] nums) {
+        int[] storage=new int[nums.length];
+       for(int i=0;i<nums.length;i++){
+        storage[i]=nums[i]*nums[i];
+        
+       }
+       Arrays.sort(storage);
+       return storage;
+    }
+    public static void main(String [] args){
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+        int nums[]=new int[n];
+        for(int i=0;i<=n-1;i++){
+            nums[i]=sc.nextInt();
+
+        }
+        arraymultiplesquare so=new arraymultiplesquare();
+       bw.readLine(so.sortedSquares(nums));
+       bw.write();
+    }
+}
