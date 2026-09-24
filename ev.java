a542787afb20cccfc8128df88985907aacfd7514
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-09-24 23:42:36 +0530

    implement eve
    Fix variable name and add scanner initialization.

---
+++ ev.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class even{
+public static void main(String[] args){
+int nnum=sc.nextInt();
+if(num%2==0){
+System.out.println("Even");
+}
+else{
+System.out.println("Odd");
+}
