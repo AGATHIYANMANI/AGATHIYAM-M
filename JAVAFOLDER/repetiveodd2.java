a086b1b748899db7a168d871072ad7719c246a93
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-09-25 14:19:31 +0530

    Rename class from repetiveodd3 to repetiveodd2

---
+++ JAVAFOLDER/repetiveodd2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class repetiveodd2 {
+    public boolean threeConsecutiveOdds(int[] arr) {
+       
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1){
+                return true;
+            } 
+        }
+            return false;
+    }
+  public static void main(String[] args){
+
+    Console cs=System.console();
+    int n=Integer.parseInt(cs.readLine());
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=Integer.parseInt(cs.readLine());
+      
+}
+    repetiveodd ro=new repetiveodd();
+    System.out.println(ro.threeConsecutiveOdds(arr));
+  }
+}
+
