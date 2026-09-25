fd48e34e0931f2849136fe4bc1e846d814ec2b8f
AGATHIYAN M <agathiyanhosur@gmail.com>
2026-09-25 14:25:43 +0530

    An integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.using console method
    Fixed the loop increment in the threeConsecutiveOdds method.

--- JAVAFOLDER/repetiveodd2.java
+++ JAVAFOLDER/repetiveodd2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 class repetiveodd2 {
     public boolean threeConsecutiveOdds(int[] arr) {
-       
+    
         for(int i=0;i<arr.length-2;i++){
             if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1){
                 return true;
